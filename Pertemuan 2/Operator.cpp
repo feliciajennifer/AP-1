456457b173898b18e4e60fa1d04ecff8f879e516
FeliciaJenniferFS-231401046 <151598729+FeliciaJenniferFS-231401046@users.noreply.github.com>
2024-03-19 09:35:37 +0700

    Pertemuan 2:

---
+++ Pertemuan 2/Operator.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+
+
+{
+    int a, b;
+
+    //asignment operator
+    a = 3;
+    b = 5;
+
+//OPERATOR ARITMATIKA 
+    // int tambah = a + b;
+    // int kurang = a - b;
+    // int kali = a * b;
+    // float bagi = (float) a / (float) b; //harus ditambahi float pada a dan b agar outputnya berkoma atau tidak nol
+    // int modulo = a % b;
+
+    // cout << "Hasil penjumlahan = " << tambah << endl;
+    // cout << "Hasil pengurangan = " << kurang << endl;
+    // cout << "Hasil perkalian = " << kali << endl;
+    // cout << "Hasil pembagian = " << bagi << endl;
+    // cout << "Hasil sisa bagi = " << modulo << endl;
+    
+//OPERATOR RASIONAL
+    // cout << (a == b) << endl;
+    // cout << (a > b) << endl;
+    // cout << (a >= b) << endl;
+    // cout << (a < b) << endl;
+    // cout << (a <= b) << endl;
+    // cout << (a != b) << endl;
+
+//OPERATOR LOGIKA
+    // cout << (true && true) << endl;
+    // cout << (true && false) << endl;
+    // cout << (false && true) << endl;
+    // cout << (false && false) << endl;
+
+    // cout << (true || true) << endl;
+    // cout << (true || false) << endl;
+    // cout << (false || true) << endl;
+    // cout << (false || false) << endl;
+
+    // cout << (!true) << endl;
+    // cout << (!false) << endl;
+
+//OPERATOR BITWISE
+    // cout << (5 & 7) << endl;
+    // cout << (5 | 7) << endl;
+    // cout << (5 ^ 7) << endl;
+    // cout << (~7) << endl;
+    // cout << (7 << 2) << endl;
+    // cout << (7 >> 2) << endl;
+    
+//SHORTHAND
+    a += 7; // sama dengan a = a + 7
+    cout << a << endl;
+
+    a -= 7;
+    cout << a << endl;
+}
